f2f393fa8e1477f386f00fcb12fde088fb72f568
Israt Ahmed <israt.jahan237@gmail.com>
2022-08-11 16:55:05 +0100

    messages input changed

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class WhatToWear {
     public static void main(String[] args) {
 
+        String setWeather = "Sunny";
+        System.out.println("What is the weather like now?");
+
         Scanner reader1 = new Scanner(System.in);
         String weather = reader1.nextLine();
+
+        int setDegree = 20;
+        System.out.println("How many degrees is it now?");
 
         Scanner reader2 = new Scanner(System.in);
         int degrees = reader2.nextInt();
 
-        if ((weather.equals("sunny") || weather.equals("cloudy"))&& (degrees > 20)){
+        if ((weather.equals("sunny") || weather.equals("cloudy"))&& (degrees > setDegree)){
             System.out.println("Wear lighter clothes!");
-        } else if (weather.equals("rainy") || weather.equals("cloudy") && (degrees < 20)){
+        } else if (weather.equals("rainy") || weather.equals("cloudy") && (degrees < setDegree)){
             System.out.println("Wear a waterproof winter coat");
         } else {
             System.out.println("Do not go out, it is too cold!");
